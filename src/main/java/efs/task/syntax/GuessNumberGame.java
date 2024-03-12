a902441a461cd1951b2d73eda2968f8c4bee70ad
pscieszka <piotr.scieszka.epinote@gmail.com>
2024-03-12 21:34:52 +0100

    test

--- src/main/java/efs/task/syntax/GuessNumberGame.java
+++ src/main/java/efs/task/syntax/GuessNumberGame.java
@@ -2,6 +2,8 @@ package efs.task.syntax;
 
 public class GuessNumberGame {
 
+    private int number;
+    private int attempts;
     //Do not modify main method
     public static void main(String[] args) {
         try {
@@ -13,10 +15,38 @@ public class GuessNumberGame {
     }
 
     public GuessNumberGame(String argument) {
-        //TODO: Implement the constructor
+        try {
+            number = Integer.parseInt(argument);
+            if(number < 1  || number > UsefulConstants.MAX_UPPER_BOUND){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        catch (NumberFormatException e) {
+            System.out.println("'" + argument + "' to " + UsefulConstants.WRONG_ARGUMENT +  " - to nie liczba");
+            System.exit(1);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(number + " to " + UsefulConstants.NOT_A_NUMBER + " - jest spoza zakresu <1," + UsefulConstants.MAX_UPPER_BOUND + ">");
+            System.exit(1);
+        }
     }
 
     public void play() {
-        //TODO: Implement the method that executes the game session
+        System.out.println("Zagrajmy. Zgadnij liczbę z zakresu <1," + number + ">");
+        attempts = calculateAttempts(number);
     }
+
+    private int calculateAttempts(int number){
+        int attempts = Math.abs((int)(Math.log(number) / Math.log(2))) + 1;
+        // Rzutuję to do int czyli tracę część ułamkową, (nie było sprecyzowane w poleceniu).
+        return attempts;
+    }
+
+    private void printGuesses(int Guesses){
+        int dots = att
+        System.out.println();
+    }
+
+
 }
